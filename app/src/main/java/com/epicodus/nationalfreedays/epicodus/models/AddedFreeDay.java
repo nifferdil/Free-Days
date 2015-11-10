9f6ad193902cad7b18493812464782b7b8235c6f
nifferdil <nifferdil@gmail.com>
2015-11-09 16:42:38 -0800

    click through to add new free day

--- app/src/main/java/com/epicodus/nationalfreedays/epicodus/models/AddedFreeDay.java
+++ app/src/main/java/com/epicodus/nationalfreedays/epicodus/models/AddedFreeDay.java
@@ -1,15 +1,12 @@
 package com.epicodus.nationalfreedays.epicodus.models;
 
 import android.app.Activity;
-import android.os.Message;
 import android.util.Log;
-import android.widget.EditText;
 
 import com.parse.FindCallback;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,11 +20,11 @@ public class AddedFreeDay {
     String mNewFreeDayDate;
     String mNewFreeDayDescription;
 
-    public void AddedFreeDay() {
+    public AddedFreeDay() {
         ParseObject parseObject = new ParseObject("AddedFreeDay");
     }
 
-    public void AddedFreeDay(String newFreeDayTitleText, String newFreeDayDate, String newFreeDayDescription, String newFreeDayWebsite) {
+    public AddedFreeDay(String newFreeDayTitleText, String newFreeDayDate, String newFreeDayDescription) {
         mNewFreeDayTitleText = newFreeDayTitleText;
         mNewFreeDayDate = newFreeDayDate;
         mNewFreeDayDescription = newFreeDayDescription;
@@ -72,18 +69,15 @@ public class AddedFreeDay {
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("AddedFreeDay");
         query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> objects, ParseException e) {
+            public void done(List<ParseObject> objects, com.parse.ParseException e) {
                 if (e == null) {
-                    for(int i = 0; i < objects.size(); i++){
-                        ParseObject pMessage = objects.get(i);
+                    for (int i = 0; i < objects.size(); i++) {
+                        ParseObject pAddedFreeDay = objects.get(i);
 
-                        AddedFreeDay addedFreeDay = new AddedFreeDay(pAddedFreeDay.getString("mNewFreeDayTitleText"),
-                        AddedFreeDay addedFreeDay = new AddedFreeDay(pAddedFreeDay.getString("mNewFreeDayDate"),
-                        AddedFreeDay addedFreeDay = new AddedFreeDay(pAddedFreeDay.getString("mNewFreeDayDescription"),
+                        AddedFreeDay addedFreeDay = new AddedFreeDay(pAddedFreeDay.getString("newFreeDayTitleText"), pAddedFreeDay.getString("newFreeDayDate"), pAddedFreeDay.getString("newFreeDayDescription"));
                         addedFreeDays.add(addedFreeDay);
 
                         context.runOnUiThread(runnable);
-
                     }
                 } else {
                     Log.d("parse", "failed request!" + e);
--- app/src/main/java/com/epicodus/nationalfreedays/epicodus/ui/NewFreeDayActivity.java
+++ app/src/main/java/com/epicodus/nationalfreedays/epicodus/ui/NewFreeDayActivity.java
@@ -1,21 +1,41 @@
 package com.epicodus.nationalfreedays.epicodus.ui;
 
-import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.epicodus.nationalfreedays.R;
+import com.epicodus.nationalfreedays.epicodus.models.AddedFreeDay;
 
-import java.util.ArrayList;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class NewFreeDayActivity extends ListActivity {
+public class NewFreeDayActivity extends AppCompatActivity {
 
-    private ArrayList mNewFreeDays;
+    @Bind(R.id.newFreeDayTitleText)TextView mNewFreeDayTitleText;
+    @Bind(R.id.newFreeDayDate)TextView mNewFreeDayDate;
+    @Bind(R.id.newFreeDayDescription)TextView mNewFreeDayDescription;
+    @Bind(R.id.addedFreeDayButton)Button mAddedFreeDayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_free_day);
+        ButterKnife.bind(this);
+
+        mAddedFreeDayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newFreeDayTitleText = mNewFreeDayTitleText.getText().toString();
+                String newFreeDayDate = mNewFreeDayDate.getText().toString();
+                String newFreeDayDescription = mNewFreeDayDescription.getText().toString();
+                AddedFreeDay(newFreeDayTitleText, newFreeDayDate, newFreeDayDescription);
+                Intent intent = new Intent(NewFreeDayActivity.this, AddedFreeDayDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
